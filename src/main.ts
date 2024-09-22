b6a607286dd599a8c155b8062fa09cd5e7aef008
Andrey Eferemov <samo.dur.andre@gmail.com>
2024-09-22 17:50:11 +0300

    добавил firebase

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 
+import { initializeApp } from 'firebase/app'
+
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
@@ -11,6 +13,17 @@ import Menubar from 'primevue/menubar'
 
 import App from './App.vue'
 import router from './router'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDO8LI8J5pH1yriNCB24edPIbt4D95UGbE',
+  authDomain: 'list-interview-cc7ea.firebaseapp.com',
+  projectId: 'list-interview-cc7ea',
+  storageBucket: 'list-interview-cc7ea.appspot.com',
+  messagingSenderId: '865704037402',
+  appId: '1:865704037402:web:497331ec5ec8382ca155a3'
+}
+
+initializeApp(firebaseConfig)
 
 const app = createApp(App)
 
--- src/views/AuthPage.vue
+++ src/views/AuthPage.vue
@@ -1,3 +1,32 @@
 <script setup lang="ts"></script>
 
-<template>Auth Page</template>
+<template>
+  <app-toast position="bottom-right" />
+  <div class="flex justify-content-center p-2">
+    <div class="surface-card p-4 shadow-2 border-round w-full lg:w-6">
+      <div class="text-center mb-3">
+        <div class="text-900 text-3xl font-medium mb-3">Приветствую!</div>
+        <span class="text-600 font-medium line-height-3">{{ subtitleText }}</span>
+        <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer" @click="toggleAuth">
+          {{ linkAccountText }}
+        </a>
+      </div>
+
+      <form @submit.prevent="submitForm">
+        <label for="email1" class="block text-900 font-medium mb-2">Email</label>
+        <app-input-text v-model="email" id="email1" type="email" class="w-full mb-3" />
+
+        <label for="password1" class="block text-900 font-medium mb-2">Пароль</label>
+        <app-input-text v-model="password" id="password1" type="password" class="w-full mb-3" />
+
+        <app-button
+          :label="submitButtonText"
+          type="submit"
+          icon="pi pi-user"
+          :loading="isLoading"
+          class="w-full"
+        ></app-button>
+      </form>
+    </div>
+  </div>
+</template>
